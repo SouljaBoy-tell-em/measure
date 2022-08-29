67c1758ac4fa5e175c56f9a0576c3a414b3b449c
Big Bill <60592559+sashascul@users.noreply.github.com>
2022-08-30 05:03:38 +0600

    Create lampOnOff.py

---
+++ lampOnOff.py
@@ -0,0 +1,12 @@
+import time
+import RPi.GPIO as GPIO
+leds = [21, 20, 16, 12, 7, 8, 25, 24]
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(leds, GPIO.OUT)
+for i in range(3):
+    for j in range(8):
+        GPIO.output(leds[j], 1)
+        time.sleep(0.2)
+        GPIO.output(leds[j], 0)
+GPIO.output(leds, 0)    
+GPIO.cleanup()
